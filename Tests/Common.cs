262804bec2fa709c14dcef57f778c7be7282f433
bojko108 <bojko108@gmail.com>
2019-02-22 21:04:17 +0200

    evaluate dS instead of dx and dy in tests

--- Tests/Common.cs
+++ Tests/Common.cs
@@ -10,17 +10,15 @@ namespace BojkoSoft.Transformations.Tests
 {
     public static class Common
     {
-        public static readonly double DELTA_OLD_BGS = 0.2;          // 10cm
+        public static readonly double DELTA_BGS = 0.1;              // 10cm
         public static readonly double DELTA_METERS = 0.01;          //  1cm
         public static readonly double DELTA_DEGREES = 0.0000001;    //  1cm
 
         public static void CheckResults(GeoPoint expected, GeoPoint result, double delta, bool checkZ = false)
         {
             double deltaX = Math.Abs(expected.X - result.X);
-            Assert.IsTrue(deltaX <= delta, "Latitude, Northing or X is not calculated correctly");
-
             double deltaY = Math.Abs(expected.Y - result.Y);
-            Assert.IsTrue(deltaY <= delta, "Longitude, Easting or Y is not calculated correctly");
+            double dS = Math.Sqrt(deltaX * deltaX + deltaY * deltaY);
 
             if (checkZ)
             {
@@ -30,7 +28,7 @@ namespace BojkoSoft.Transformations.Tests
             }
             else
             {
-                Console.WriteLine(String.Format("expected: {0}\nreceived: {1}\ndeltaX: {2}\ndeltaY: {3}", expected.ToString(), result.ToString(), deltaX.ToString(), deltaY.ToString()));
+                Console.WriteLine(String.Format("expected: {0}; result: {1}\ndS: {2}", expected, result, dS));
             }
         }
     }
