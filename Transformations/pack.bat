8d958f5f34f424110aead8e79474f3d9473c58cd
bojko108 <bojko108@gmail.com>
2019-09-04 15:50:31 +0300

    update version

--- Transformations/pack.bat
+++ Transformations/pack.bat
@@ -1,8 +1,8 @@
 rem update version
+rem update releaseNotes in nuspec file
 rem build
 rem run tests
 rem git push
-rem update releaseNotes
 rem and then
 "c:\nuget.exe" pack Transformations.csproj -IncludeReferencedProjects
 pause
