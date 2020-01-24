ea5bb19bdc98fef4934113941fc2ccdf795f5c01
bojko108 <bojko108@gmail.com>
2020-01-24 16:38:14 +0200

    Add methods to save transformation parameters

--- Transformations/TransformationModels/AffineTransformation.cs
+++ Transformations/TransformationModels/AffineTransformation.cs
@@ -9,15 +9,47 @@ namespace BojkoSoft.Transformations.TransformationModels
     /// <summary>
     /// Class for transforming points using Affine transformation
     /// </summary>
-    internal class AffineTransformation : ITransformation
+    public class AffineTransformation : ITransformation
     {
-        private double a1 { get; set; }
-        private double a2 { get; set; }
-        private double b1 { get; set; }
-        private double b2 { get; set; }
-        private double c1 { get; set; }
-        private double c2 { get; set; }
-        
+        /// <summary>
+        /// x-component of the pixel width (x-scale)
+        /// </summary>
+        public double A { get; private set; }
+        /// <summary>
+        /// y-component of the pixel height (y-scale), typically negative
+        /// </summary>
+        public double E { get; private set; }
+        /// <summary>
+        /// rotation about x-axis
+        /// </summary>
+        public double B { get; private set; }
+        /// <summary>
+        /// y-component of the pixel width (y-skew)
+        /// </summary>
+        public double D { get; private set; }
+        /// <summary>
+        /// x-coordinate of the center of the upper left pixel
+        /// </summary>
+        public double C { get; private set; }
+        /// <summary>
+        /// y-coordinate of the center of the upper left pixel
+        /// </summary>
+        public double F { get; private set; }
+
+        /// <summary>
+        /// Creates a new Affine transformation
+        /// </summary>
+        public AffineTransformation() : this(new double[6] { 1, 1, 1, 1, 0, 0 }) { }
+
+        /// <summary>
+        /// Creates a new Affine transfromation with provided parameters
+        /// </summary>
+        /// <param name="parameters">List of transformation parameters - https://en.wikipedia.org/wiki/World_file#Definition</param>
+        public AffineTransformation(double[] parameters)
+        {
+            this.SetParameters(parameters);
+        }
+
         /// <summary>
         /// Creates a new Affine transformation based on input source and target points
         /// </summary>
@@ -35,11 +67,61 @@ namespace BojkoSoft.Transformations.TransformationModels
         /// <returns></returns>
         public GeoPoint Transform(GeoPoint inputPoint)
         {
-            GeoPoint resultPoint = new GeoPoint();
-            resultPoint.X = this.a1 * inputPoint.X + this.b1 * inputPoint.Y + this.c1;
-            resultPoint.Y = this.b2 * inputPoint.X + this.a2 * inputPoint.Y + this.c2;
+            GeoPoint resultPoint = new GeoPoint
+            {
+                X = this.A * inputPoint.X + this.B * inputPoint.Y + this.C,
+                Y = this.D * inputPoint.X + this.E * inputPoint.Y + this.F
+            };
             return resultPoint;
         }
+
+        /// <summary>
+        /// Returns calculated parameters. Use this method to save the prameters for later use.
+        /// </summary>
+        /// <returns>Affine transformation parameters</returns>
+        public double[] GetParameters()
+        {
+            return new double[6] { this.A, this.E, this.B, this.D, this.C, this.F };
+        }
+
+        /// <summary>
+        /// Returns calculated parameters. Use this method to save the prameters in a world file - https://en.wikipedia.org/wiki/World_file
+        /// </summary>
+        /// <returns>parameters as text</returns>
+        public string GetParametersAsText()
+        {
+            return $"{this.A}\n{this.E}\n{this.B}\n{this.D}\n{this.C}\n{this.F}";
+        }
+
+        /// <summary>
+        /// Sets the transformation parameters
+        /// </summary>
+        /// <param name="parameters">List of transformation parameters (A, E, B, D, C, F) - https://en.wikipedia.org/wiki/World_file#Definition</param>
+        public void SetParameters(double[] parameters)
+        {
+            if (parameters.Length != 6)
+            {
+                throw new ArgumentException("Length should be 6: A, E, B, D, C, F");
+            }
+
+            this.A = parameters[0];
+            this.E = parameters[1];
+            this.B = parameters[2];
+            this.D = parameters[3];
+            this.C = parameters[4];
+            this.F = parameters[5];
+        }
+
+        /// <summary>
+        /// Sets the transformation parameters from provided workd file text - https://en.wikipedia.org/wiki/World_file
+        /// </summary>
+        /// <param name="text">world file text</param>
+        public void SetParameters(string text)
+        {
+            string[] parameters = text.Split(new char[1] { '\n' }, StringSplitOptions.RemoveEmptyEntries);
+            this.SetParameters(Array.ConvertAll(parameters, Double.Parse));
+        }
+
         /// <summary>
         /// Calculates Affine transformation parameters
         /// </summary>
@@ -85,17 +167,17 @@ namespace BojkoSoft.Transformations.TransformationModels
                 y3 += sourcePoints[i].Y * sourcePoints[i].Y;
                 x4 += sourcePoints[i].X * sourcePoints[i].Y;
             }
-            this.a1 = (x1 * y3) - (y1 * x4);
-            this.b1 = (y1 * x2) - (x1 * x4);
-            this.a2 = (y2 * x2) - (x3 * x4);
-            this.b2 = (x3 * y3) - (y2 * x4);
+            this.A = (x1 * y3) - (y1 * x4);
+            this.B = (y1 * x2) - (x1 * x4);
+            this.E = (y2 * x2) - (x3 * x4);
+            this.D = (x3 * y3) - (y2 * x4);
             n1 = (x2 * y3) - (x4 * x4);
-            this.a1 /= n1;
-            this.b1 /= n1;
-            this.a2 /= n1;
-            this.b2 /= n1;
-            this.c1 = xcg - (this.a1 * xcl) - (this.b1 * ycl);
-            this.c2 = ycg - (this.a2 * ycl) - (this.b2 * xcl);
+            this.A /= n1;
+            this.B /= n1;
+            this.E /= n1;
+            this.D /= n1;
+            this.C = xcg - (this.A * xcl) - (this.B * ycl);
+            this.F = ycg - (this.E * ycl) - (this.D * xcl);
         }
     }
 }
