814299dea17970275c3039e8d090b673b2b786a9
bojko108 <bojko108@gmail.com>
2021-10-28 13:53:09 +0300

    sign the assembly

--- Transformations/Properties/AssemblyInfo.cs
+++ Transformations/Properties/AssemblyInfo.cs
@@ -35,4 +35,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyVersion("4.0.0.0")]
 [assembly: AssemblyFileVersion("4.0.0.0")]
 
-[assembly: InternalsVisibleTo("Tests")]
+[assembly: InternalsVisibleTo("Tests,PublicKey=" +
+    "0024000004800000940000000602000000240000525341310004000001000100c5b8ab9e2fa106" +
+    "2eee275201468ebaea7205efc505229423634e6b77dace9ccccbd95884778601e031c1e1a61701" +
+    "c336fd693dd5fbec4c1aa0259a5fe2b63844a286cb0f905a7d3ba6ddc368cce2336312ea11f0d4" +
+    "c78562abb7a29433599722742fc77580bc62fb1f06945fb2c2df1dcf8b43d259a28f67e7bf8197" +
+    "fe5c4cca")]
